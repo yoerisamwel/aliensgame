e2f96fbc5a658fccd5cbd13375d650f94a489b73
yoerisamwel <39857716+yoerisamwel@users.noreply.github.com>
2019-09-19 11:11:33 -0400

    simplifying code
    Following book instructions

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,17 +43,24 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    self._check_keydown_events(event)
+                    
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
-                        #move the ship to the right
-                        self.ship.rect.x += 1
+                    self._check_keyup_events(event)
+                        
+    def _check_keydown_events(self, event):
+         if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = True
+         elif event.key == pygame.K_LEFT:
+             self.ship.moving_left = True
+                        
+    def _check_keyup_events(self, event):
+         if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = False
+         elif event.key == pygame.K_LEFT:
+             self.ship.moving_left = False
+
+
                     
         #redraw the screen during each pass through the loop
    
