8d3a639719cfecb89b399f5ed098fe3ca2d8d074
Yoeri Samwel <yoerisamwel@gmail.com>
2022-03-27 18:04:57 -0400

    Update on the aliens not going over the edge of the screen

--- alien.py
+++ alien.py
@@ -31,6 +31,12 @@ class Alien(Sprite):
 
     def update(self):
         """move the aliens to the right"""
-        self.x += self.settings.alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
-        
+
+    def check_edges(self):
+        """Return true if alien is at the edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
